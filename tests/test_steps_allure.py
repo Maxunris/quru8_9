610900df19b6e7be1aa70b8746f034f5b9431b9c
max <Maxunris@gmail.com>
2023-10-16 19:35:19 +0300

    selene steps

--- tests/test_steps_allure.py
+++ tests/test_steps_allure.py
@@ -6,13 +6,17 @@ from selene import browser, by, be
 
 def test_steps_allure(settings_browser):
     with allure.step('Открываем главную страницу Гитхаба'):
-        browser.open('https://github.com/').element('.search-input').click()
+        browser.open('https://github.com/')
 
-    with allure.step('поиск репозитория'):
+    with allure.step('Поиск репозитория'):
+        browser.element('.search-input').click()
         browser.element('#query-builder-test').send_keys('Maxunris/quru8_9').press_enter()
+
+    with allure.step('Заходим в репозиторий'):
         browser.element(by.link_text('Maxunris/quru8_9')).click()
 
-    browser.element('#issues-tab').click()
-    browser.element(by.partial_text('test issue')).should(be.visible)
+    with allure.step('Нажимаем на Issues'):
+        browser.element('#issues-tab').click()
 
-    browser.quit()
+    with allure.step('Проверка наличия кнопки Issues'):
+        browser.element(by.partial_text('test issue')).should(be.visible)
