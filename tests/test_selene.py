cd8ac1d66ecce9694917f69179b635478965f411
max <Maxunris@gmail.com>
2023-10-16 19:29:06 +0300

    selene

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -5,6 +5,8 @@ def test_with_selene_issue(settings_browser):
     browser.open('https://github.com/').element('.search-input').click()
     browser.element('#query-builder-test').send_keys('Maxunris/quru8_9').press_enter()
     browser.element(by.link_text('Maxunris/quru8_9')).click()
+
     browser.element('#issues-tab').click()
     browser.element(by.partial_text('test issue')).should(be.visible)
+    
     browser.quit()
--- tests/test_steps_allure.py
+++ tests/test_steps_allure.py
@@ -1,7 +1,18 @@
+import allure
+from selene import browser, by, be
 
 
 
 
+def test_steps_allure(settings_browser):
+    with allure.step('Открываем главную страницу Гитхаба'):
+        browser.open('https://github.com/').element('.search-input').click()
 
-def test_steps_allure():
-    pass
+    with allure.step('поиск репозитория'):
+        browser.element('#query-builder-test').send_keys('Maxunris/quru8_9').press_enter()
+        browser.element(by.link_text('Maxunris/quru8_9')).click()
+
+    browser.element('#issues-tab').click()
+    browser.element(by.partial_text('test issue')).should(be.visible)
+
+    browser.quit()
